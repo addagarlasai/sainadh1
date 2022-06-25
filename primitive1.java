ca3a72964661a0e84d032cbb7f8ef0f8c32ac332
addagarlasai <saiaddagarla@gmail.com>
2022-06-25 07:51:06 +0530

    fristtt

--- primitive1.java
+++ primitive1.java
@@ -4,7 +4,7 @@ public class primitive1
 {
     public static void main(String[] args)
     {
-    	int max = 2147483647;
+    	
 		int min = -2147483648;
 		short shortMax = 32767;
 		short shortMin = -32768;
